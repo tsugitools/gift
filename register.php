a5221c8fd834b1c873ccbb033763c7a8597956c4
Charles Severance <csev@umich.edu>
2017-12-27 23:16:22 -0500

    Improve the store detail.

--- register.php
+++ register.php
@@ -4,7 +4,36 @@ $REGISTER_LTI2 = array(
 "name" => "Quizzes",
 "FontAwesome" => "fa-question-circle",
 "short_name" => "Quizzes",
-"description" => "This tool provides a quiz engine that supports the GIFT format. GIFT is a line-oriented plain text question format that is simple to understand and esaily edited by hand or even stored in a repository like github.",
-"messages" => array("launch", "launch_grade")
+"description" => "This tool provides a quiz engine that supports the GIFT format.
+GIFT is a line-oriented plain text question format that is simple to understand
+and esaily edited by hand or even stored in a repository like github.",
+"messages" => array("launch", "launch_grade"),
+ "privacy_level" => "anonymous",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "analytics" => array(
+        "internal"
+    ),
+    "source_url" => "https://github.com/tsugitools/gift",
+    "placements" => array(
+        "course_navigation", "homework_submission"
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+        "store/screen-01.png",
+        "store/screen-02.png",
+        "store/screen-03.png",
+        "store/screen-04.png",
+        "store/screen-05.png",
+        "store/screen-06.png",
+        "store/screen-analytics.png"
+    )
 );
 
