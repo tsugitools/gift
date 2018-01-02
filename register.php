81a709132bb5a92eff1109fd9a6aa55eb4548d21
Charles Severance <csev@umich.edu>
2018-01-02 18:03:15 -0500

    Fix typo

--- register.php
+++ register.php
@@ -6,7 +6,7 @@ $REGISTER_LTI2 = array(
 "short_name" => "Quizzes",
 "description" => "This tool provides a quiz engine that supports the GIFT format.
 GIFT is a line-oriented plain text question format that is simple to understand
-and esaily edited by hand or even stored in a repository like github.",
+and easily edited by hand or even stored in a repository like github.",
 "messages" => array("launch", "launch_grade"),
  "privacy_level" => "anonymous",  // anonymous, name_only, public
     "license" => "Apache",
