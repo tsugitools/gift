985292b7cde0a5d2f90f10c98a217ed8f817aada
jonhartm <jonhartm@umich.edu>
2018-04-19 12:42:30 -0400

    start of template-based quiz authoring

--- configure.php
+++ configure.php
@@ -1,7 +1,6 @@
 <?php
 require_once "../config.php";
-require_once "parse.php";
-require_once "sample.php";
+require_once "templates/authoring.php";
 
 use \Tsugi\Core\Cache;
 use \Tsugi\Core\LTIX;
@@ -13,142 +12,49 @@ if ( ! $USER->instructor ) die("Requires instructor role");
 // Model
 $p = $CFG->dbprefix;
 
-// If they pressed Submit on the quiz content
-if ( isset($_POST['gift']) ) {
-    $gift = $_POST['gift'];
-    $_SESSION['gift'] = $gift;
-
-    // Some sanity checking...
-    $retval = check_gift($gift);
-    if ( ! $retval ) {
-        header( 'Location: '.addSession('configure.php') ) ;
-        return;
-    }
-
-    // This is not JSON - no one cares
-    $LINK->setJson($gift);
-    $_SESSION['success'] = 'Quiz updated';
-    unset($_SESSION['gift']);
-    header( 'Location: '.addSession('index.php') ) ;
-    return;
-}
-
-// Check to see if we are supposed to preload a quiz
-$files = false;
-$lock = false;
-if ( isset ($CFG->giftquizzes) && is_dir($CFG->giftquizzes) ) {
-    $files1 = scandir($CFG->giftquizzes);
-    $files = array();
-    foreach($files1 as $file) {
-        if ( $file == '.lock' ) {
-            $lock = trim(file_get_contents($CFG->giftquizzes.'/'.$file));
-            continue;
-        }
-        if ( strpos($file, '.') === 0 ) continue;
-        $files[] = $file;
-    }
-    sort($files);
-}
-if ( count($files) < 1 ) {
-    $_SESSION['error'] = "Found no files in ".$CFG->giftquizzes;
-    header( 'Location: '.addSession('configure.php') ) ;
-    return;
-}
-// print_r($files);
-// echo("LOCK = ".$lock);
-
-$default = isset($_SESSION['default_quiz']) ? $_SESSION['default_quiz'] : false;
-
-// Load up the selected file
-if ( $files && isset($_POST['file']) ) {
-    $key = isset($_POST['lock']) ? $_POST['lock'] : false;
-    if ( $lock && $lock != $key ) {
-        $_SESSION['error'] = 'Incorrect password';
-        header( 'Location: '.addSession('configure.php') ) ;
-        return;
-    }
-
-    $name = $_POST['file'];
-    if ( ! in_array($name, $files) ) {
-        $_SESSION['error'] = 'Quiz file not found: '.$_POST['file'];
-        header( 'Location: '.addSession('configure.php') ) ;
-        return;
-    }
-
-    $gift = file_get_contents($CFG->giftquizzes.'/'.$name);
-    $_SESSION['gift'] = $gift;
-
-    // Also pre-check for sanity
-    $retval = check_gift($gift);
-    if ( ! $retval ) {
-        header( 'Location: '.addSession('configure.php') ) ;
-        return;
-    }
-
-    $_SESSION['success'] = 'Preloaded quiz content from file. Make sure to save the quiz below.';
-    header( 'Location: '.addSession('configure.php') ) ;
-    return;
-}
-
-// Load up the quiz from session or DB
-if ( isset($_SESSION['gift']) ) { 
-    $gift = $_SESSION['gift'];
-    unset($_SESSION['gift']);
-} else {
-    $gift = $LINK->getJson();
-}
-
-// Clean up the JSON for presentation
-if ( $gift === false || strlen($gift) < 1 ) {
-    if ( $default != false && $lock == false && in_array($default, $files) ) {
-        $gift = file_get_contents($CFG->giftquizzes.'/'.$default);
-        $_SESSION['success'] = 'Loaded quiz '.$default.' as default';
-    } else {
-        $gift = getSampleGIFT();
-    }
-}
-
 // View
 $OUTPUT->header();
+?>
+<link rel="stylesheet" type="text/css" href="css/authoring.css">
+<?php
 $OUTPUT->bodyStart();
 $OUTPUT->topNav();
 $OUTPUT->flashMessages();
 ?>
-<p>Be careful in making any changes if this quiz has submissions.</p>
-<?php 
-if ( $files !== false ) {
-echo("<form method=\"post\">\n");
-// echo('<select name="file" onchange="console.dir(this); if(this.value!=0) this.form.submit();">'."\n");
-echo('<select name="file">'."\n");
-echo('<option value="0">Select Quiz</option>'."\n");
-foreach($files as $file) {
-    if ( $default && $default == $file ) {
-        echo('<option value="'.htmlentities($file).'" selected>'.htmlentities($file).'</option>'."\n");
-    } else {
-        echo('<option value="'.htmlentities($file).'">'.htmlentities($file).'</option>'."\n");
-    }
-}
-echo("</select>\n");
-if ( $lock != false ) {
-    echo('<input type="password" name="lock"> Password ');
-}
-echo('<input type="submit" value="Load Quiz">');
-echo("</form>\n");
-}
-?>
-<p>
-The assignment is configured by carefully editing the gift below.
-The documentation for the GIFT format comes from 
-<a href="https://docs.moodle.org/29/en/GIFT_format" target="_blank">Moodle Documentation</a>.
-</p>
-<form method="post" style="margin-left:5%;">
-<textarea name="gift" rows="25" cols="80" style="width:95%" >
-<?php echo(htmlent_utf8($gift)); ?>
-</textarea>
-<p>
+<form method="post">
+<div id="quiz_content"></div>
+</form>
+<select id="question_type_select">
+  <option value=""> -- Add a New Question -- </option>
+  <option value="true_false_question">True/False Question</option>
+  <option value="multiple_choice_question">Multiple Choice/Multiple Answer Question</option>
+  <option value="short_answer_question">Short Answer Question</option>
+</select><br>
 <input type="submit" value="Save">
 <input type=submit name=doCancel onclick="location='<?php echo(addSession('index.php'));?>'; return false;" value="Cancel"></p>
-</form>
+<input type=submit name=view onclick="location='<?php echo(addSession('quiz_data.php'));?>'; return false;" value="View JSON"></p>
 <?php
-
 $OUTPUT->footer();
+$OUTPUT->templateInclude(array('common', 'tf_authoring', 'mc_authoring', 'sa_authoring'));
+?>
+<script type="text/javascript" src="js/author.js"></script>
+<script>
+
+question_number = 0;
+
+$(document).ready(()=> {
+  // see if there's already a quiz saved in the JSON
+  $.getJSON("<?= addSession('quiz_data.php') ?>", function(quizData) {
+    if (!quizData) {
+      console.log("No quiz is configured");
+    } else {
+      for (var q=0; q<quizData.length;q++) {
+        var context = quizData[q];
+        context.count = q + 1;
+        addQuestion(context);
+      }
+      question_number = quizData.length;
+    }
+  });
+})
+</script>
---
+++ js/author.js
@@ -0,0 +1,67 @@
+function addQuestion(context) {
+  tsugiHandlebarsToDiv_noEmpty('quiz_content', 'common', context);
+  switch (context.type) {
+    case "true_false_question": addTrueFalse(context); break;
+    case "multiple_choice_question": addMultipleChoice(context); break;
+    case "multiple_answers_question": addMultipleChoice(context); break;
+    case "short_answer_question": addShortAnswer(context); break;
+    default: console.log("unrecognized question type: " + context.type);
+  }
+  lti_frameResize();
+}
+
+function addTrueFalse(context) {
+  if (context.answer == "T") {
+    context.answer_true = true;
+  } else if (context.answer == "F") {
+    context.answer_false = true;
+  }
+  tsugiHandlebarsToDiv_noEmpty("q"+context.count+"_content", 'tf_authoring', context);
+}
+
+function addMultipleChoice(context) {
+  if ("parsed_answer" in context) {
+    for (var a=0; a<context.parsed_answer.length; a++) {
+      var answer_context = {};
+      answer_context.isCorrect = context.parsed_answer[a][0];
+      answer_context.value = context.parsed_answer[a][1];
+      answer_context.count = context.count;
+      answer_context.num = a + 1;
+      tsugiHandlebarsToDiv_noEmpty("q"+context.count+"_content", 'mc_authoring', answer_context);
+    }
+  }
+}
+
+function addShortAnswer(context) {
+  if ("parsed_answer" in context) {
+    for (var a=0; a<context.parsed_answer.length; a++) {
+      var answer_context = {};
+      answer_context.value = context.parsed_answer[a][1];
+      answer_context.count = context.count;
+      answer_context.num = a + 1;
+      tsugiHandlebarsToDiv_noEmpty("q"+context.count+"_content", 'sa_authoring', answer_context);
+    }
+  }
+}
+
+$("#question_type_select").change(function() {
+  var selected_value = $("#question_type_select").val();
+  if (selected_value != "") {
+    var context = {};
+    context.count = ++question_number;
+    context.type = selected_value;
+    addQuestion(context);
+    $("#question_type_select").val("");
+  }
+});
+
+function tsugiHandlebarsToDiv_noEmpty(div, name, context) {
+  $('#'+div).append(tsugiHandlebarsRender(name, context));
+}
+
+function renumber_questions() {
+  var question_headers = $("h1");
+  for (var i=0;i<question_headers.length;i++) {
+    $(question_headers[i]).html("Question " + (i+1));
+  }
+}
