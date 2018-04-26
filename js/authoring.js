d4e6dd6df070d1d88a494216f3fc4ae3f73131fa
jonhartm <jonhartm@umich.edu>
2018-04-26 13:53:25 -0400

    combine the js files

--- js/authoring.js
+++ js/authoring.js
@@ -135,3 +135,13 @@ function renumber_questions() {
     $(questions[i]).html(new_html);
   }
 }
+
+// -----------------------------------------------------------------------------
+//                        KEYBOARD NAV FUNCTIONS
+// -----------------------------------------------------------------------------
+
+// Set the cursor focus on the last text box in the given div
+// Used when the user presses the "+" button to add a new answer option
+function set_focus_on_lastinput(sender) {
+  $(sender + " div:last-child input[type=text]").focus();
+}
--- js/authoring_keyboard_nav.js
+++
@@ -1,5 +0,0 @@
-// Set the cursor focus on the last text box in the given div
-// Used when the user presses the "+" button to add a new answer option
-function set_focus_on_lastinput(sender) {
-  $(sender + " div:last-child input[type=text]").focus();
-}
